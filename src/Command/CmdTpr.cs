01daca450b610bc77772806ee3ffc88cd471bc3b
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-07-26 19:21:32 -0500

    no such thing as negative coordinates? hmm..

--- src/Command/CmdTpr.cs
+++ src/Command/CmdTpr.cs
@@ -13,59 +13,32 @@ using Vintagestory.API.Util;
 namespace BasicCommands.Command;
 
 public class CmdTpr : AbstractCommand {
-    private static readonly ConcurrentDictionary<string, Vec2i> pending = new();
+    private static readonly HashSet<string> pending = new();
 
     public override TextCommandResult Execute(BasicPlayer sender, TextCommandCallingArgs args) {
-        if (pending.ContainsKey(sender.UID)) {
+        if (pending.Contains(sender.UID)) {
             return TextCommandResult.Success(Lang.Get("tpr-already-waiting"));
         }
 
         ICoreServerAPI api = BasicCommandsMod.Instance().API;
         IWorldManagerAPI worldManager = api.WorldManager;
 
-        int apothemX = worldManager.MapSizeX / 2;
-        int apothemZ = worldManager.MapSizeZ / 2;
+        Random rand = Random.Shared;
+        int randX = rand.Next(worldManager.MapSizeX);
+        int randZ = rand.Next(worldManager.MapSizeZ);
         int chunkSize = worldManager.ChunkSize;
 
-        Random rand = Random.Shared;
-        int randX = rand.Next(-apothemX, apothemX);
-        int randZ = rand.Next(-apothemZ, apothemZ);
+        pending.Add(sender.UID);
 
         worldManager.LoadChunkColumnPriority(randX / chunkSize, randZ / chunkSize, new ChunkLoadOptions() {
             OnLoaded = () => {
                 pending.Remove(sender.UID);
-                api.Logger.Event($"{randX}, {worldManager.GetSurfacePosY(randX, randZ)}, {randZ}");
-                api.Logger.Event($"aX: {apothemX} aZ: {apothemZ} cS: {chunkSize} rX: {randX} rZ: {randZ}");
-                //ProcessWaitingChunk(chunk);
+                int topY = (worldManager.GetSurfacePosY(randX, randZ) ?? 0) + 1;
+                sender.TeleportTo(new(randX, topY, randZ));
+                sender.SendMessage(Lang.Get("tpr-success", randX, topY, randZ));
             }
         });
 
-        pending.TryAdd(sender.UID, new(randX, randZ));
-
         return TextCommandResult.Success(Lang.Get("tpr-wait"));
-    }
-
-    public static void ProcessWaitingChunk(Vec2i chunk) {
-        ICoreServerAPI api = BasicCommandsMod.Instance().API;
-        api.Logger.Event($"Chunk loaded: {chunk}");
-        IWorldManagerAPI worldManager = api.WorldManager;
-        for (int i = pending.Count - 1; i >= 0; i--) {
-            KeyValuePair<string, Vec2i> entry = pending.ElementAt(i);
-            Vec2i block = entry.Value;
-            if (chunk.X != block.X / worldManager.ChunkSize || chunk.Y != block.Y / worldManager.ChunkSize) {
-                api.Logger.Event($"Chunk mismatch: {block}");
-                continue;
-            }
-            pending.Remove(entry.Key);
-            BasicPlayer player = BasicPlayer.Get(entry.Key);
-            if (player == null) {
-                api.Logger.Event("Null player");
-                continue;
-            }
-            Vec3d pos = new(block.X, (worldManager.GetSurfacePosY(block.X, block.Y) ?? 0) + 1, block.Y);
-            api.Logger.Event($"Pos: {pos}");
-            player.TeleportTo(pos);
-            player.SendMessage(Lang.Get("tpr-success", (int)pos.X, (int)pos.Y, (int)pos.Z));
-        }
     }
 }
